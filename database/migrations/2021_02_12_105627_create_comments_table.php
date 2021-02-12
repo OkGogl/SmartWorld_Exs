e5cb54d542a16626e9ae1ab4c447c52cf64d0e57
OkGogl <58540544+OkGogl@users.noreply.github.com>
2021-02-12 19:13:33 +0700

    v.2

--- database/migrations/2021_02_12_105627_create_comments_table.php
+++ database/migrations/2021_02_12_105627_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('email')->unique();
-            $table->string('login');
-            $table->string('password');
-            $table->timestamp('registration_date')->nullable();
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('post_id');
+            $table->unsignedInteger('user_id');
+            $table->string('text');
+            $table->timestamp('created_at');
+
         });
     }
 
@@ -29,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }
