198664df1b9267c82c74876fe0a25e376022051f
OkGogl <58540544+OkGogl@users.noreply.github.com>
2021-02-12 20:19:13 +0700

    Delete about.blade.php

--- about.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts')
-
-@section('title')Информация про нас@endsection
-
-@section('main_content')
-    <h1>Информация про нас</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consequuntur, culpa dicta doloribus ea error quidem reiciendis sapiente soluta voluptas. Amet culpa in laborum sit voluptatem. Consequuntur deserunt eveniet nesciunt.</p>
-@endsection
